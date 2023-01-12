a29433f05337a77e702c5a685417ed3eb46131a4
antoineTsinga <antoinetsinga@gmail.com>
2023-01-12 04:08:52 +0100

    home page

---
+++ app/src/main/java/com/streamavailability/ui/moviedetails/MovieDetails.java
@@ -0,0 +1,16 @@
+package com.streamavailability.ui.moviedetails;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.streamavailability.R;
+
+public class MovieDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_details);
+    }
+}
